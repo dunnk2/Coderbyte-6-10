2b098b1b0daf0912487967e9694d1e2c74da646d
johnsonconsulting2 <johnsonconsulting2@gmail.com>
2014-05-06 17:03:00 -0400

    Challenge 6

--- KyleandChristi/teamCoderJS.js
+++ KyleandChristi/teamCoderJS.js
@@ -1,1 +1,22 @@
-﻿
+﻿//Using the JavaScript language, have the function LetterCapitalize(str) take the str parameter being passed and capitalize 
+//the first letter of each word. Words will be separated by only one space. 
+
+//Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+function letterCapitalize(str) {
+    var myString = str.split(" ");
+
+    for (var i = 0; i < myString.length; i++) {
+        myString[i] = myString[i].charAt(0).toUpperCase() + myString[i].substr(1);
+    }
+
+     
+    return myString.join(" ");
+
+}
+
+alert(letterCapitalize("We are Happy"));
+
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+//LetterCapitalize(readline());
